cd4395c1fe513640d8dd58570e2cd9e6c9d97d50
Leaf <itzsimplyleaf345@gmail.com>
2026-01-03 19:10:09 +0100

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -5,11 +5,12 @@ app = Flask('')
 
 @app.route('/')
 def home():
-    return "Bot is awake and running!"
+    return "Status: Operational"
 
 def run():
     app.run(host='0.0.0.0', port=8080)
 
 def keep_alive():
     t = Thread(target=run)
+    t.daemon = True
     t.start()
